5715f7c2498f5c22afd00a94501f90e4d5402f9b
Mark C.C. Chiang <chcchiang@asiaa.sinica.edu.tw>
2021-02-10 00:03:18 +0800

    stop the loop after timeout

--- sample.cpp
+++ sample.cpp
@@ -5,6 +5,8 @@
 using namespace std;
 
 int main() {
+    volatile bool loop(true);
+
     Timer t = Timer();
 
     t.setInterval([&]() { cout << "Hey.. After each 1s..." << endl; }, 1000);
@@ -13,11 +15,15 @@ int main() {
         [&]() {
             cout << "Hey.. After 5.2s. But I will stop the timer!" << endl;
             t.stop();
+            cout << "End the process!" << endl;
+            loop = false;
         },
         5200);
 
     cout << "I am Timer" << endl;
 
-    while (true)
+    while (loop)
         ; // Keep mail thread active
+
+    return 0;
 }
